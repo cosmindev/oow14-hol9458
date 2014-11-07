23ace4cd5e6fb7c3c286fc9afeb821a9cb37d60d
cosmindev <cosmin.tudor82@gmail.com>
2014-11-07 12:00:02 +0200

    external commit

--- src/test/java/com/oracle/cloud/tests/AllTests.java
+++ src/test/java/com/oracle/cloud/tests/AllTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 //change
 //3rdchange
 //2ndchange
+//4thchange
 @RunWith(Suite.class)
 @SuiteClasses({ TestCustomersBean.class, TestOrdersBean.class })
 public class AllTests {
