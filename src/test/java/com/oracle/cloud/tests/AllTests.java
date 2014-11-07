de8a6a137c4c896b4e3090ef06075b8824c38cc7
cosmindev <cosmin.tudor82@gmail.com>
2014-11-07 00:39:02 -0800

    change

--- src/test/java/com/oracle/cloud/tests/AllTests.java
+++ src/test/java/com/oracle/cloud/tests/AllTests.java
@@ -3,7 +3,7 @@ package com.oracle.cloud.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+//change
 @RunWith(Suite.class)
 @SuiteClasses({ TestCustomersBean.class, TestOrdersBean.class })
 public class AllTests {
